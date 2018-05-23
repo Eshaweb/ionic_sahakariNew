444dd5448d5230f52e99f7527a2e60608016ae81
ShaileshBhatNP <shailesh81094@gmail.com>
2018-05-23 10:47:53 +0530

    recharge -report deleted due to some problems

--- src/pages/UIHelperClasses/UIHelperService.ts
+++ src/pages/UIHelperClasses/UIHelperService.ts
@@ -3,13 +3,13 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UIHelperService {
-    readonly rootUrl = 'https://sahakari.azurewebsites.net';
-    readonly baseUrl = 'https://sahakari.azurewebsites.net/api';
-    readonly rootUrl1 = 'https://sahakari.azurewebsites.net/token';
+    // readonly rootUrl = 'https://sahakari.azurewebsites.net';
+    // readonly baseUrl = 'https://sahakari.azurewebsites.net/api';
+    // readonly rootUrl1 = 'https://sahakari.azurewebsites.net/token';
 
-    // readonly rootUrl = 'http://localhost:55394';
-    // readonly baseUrl='http://localhost:55394/api';
-    // readonly rootUrl1 = 'http://localhost:55394/token';
+    readonly rootUrl = 'http://localhost:55394';
+    readonly baseUrl='http://localhost:55394/api';
+    readonly rootUrl1 = 'http://localhost:55394/token';
 
     CallWebAPIUrl(api_action_name: any) {
         return this.rootUrl + api_action_name;
